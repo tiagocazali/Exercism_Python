e8dabfddf854b9c4851f86fd6cec16cea64bf5f0
Tiago Cazali Magalhães <tiagocazali@gmail.com>
2025-08-24 21:30:17 +0200

    version 1 from Matching brackets

--- matching-brackets/matching_brackets.py
+++ matching-brackets/matching_brackets.py
@@ -1,2 +1,37 @@
 def is_paired(input_string):
-    pass
+    
+    symbols_open = ["(", "{", "["]
+    symbols_close = [")", "}", "]"]
+
+    close_to_open = {")" : "(", "}" : "{", "]" : "["}
+
+    #brackets_counter = {"[" : 0, "(" : 0, "{" : 0 }
+    brackets_order = []
+
+    for letter in input_string:
+
+        if letter in symbols_open:
+            #brackets_counter[letter] += 1
+            brackets_order.append(letter)
+        
+        if letter in symbols_close:
+            open_symbol = close_to_open[letter]
+            #brackets_counter[open_symbol] -= 1
+            if len(brackets_order) == 0 or (brackets_order.pop() != open_symbol):
+                return False
+
+            # if brackets_counter[open_symbol] < 0:
+            #     print("fechou item sem abrir")
+            #     return False
+    
+    # for quant in brackets_counter.values():
+    #     if quant != 0:
+    #         return False
+    if len(brackets_order) != 0:
+        return False
+    
+    return True
+
+
+print(is_paired("}{"))
+
