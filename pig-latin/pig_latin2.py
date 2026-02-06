526438b3c427533601ae2c9ed622ea9dfb86eecf
Tiago Cazali Magalhães <tiagocazali@gmail.com>
2026-02-06 01:21:33 +0100

    new solution for Pig Latan

---
+++ pig-latin/pig_latin2.py
@@ -0,0 +1,20 @@
+def translate(text):
+    
+    words = text.split()
+    
+    answer = pig_latin(word) for word in words
+    
+
+
+    vowels = ["a","e","i","o","u"]
+    new_text = []
+
+    if 
+
+
+
+
+print(translate("quick fast rhythm my yellow"))
+
+
+
